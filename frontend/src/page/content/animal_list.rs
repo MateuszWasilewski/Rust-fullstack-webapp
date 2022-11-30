0d2a4a0dbe01a75a23204e93c3bb98787ac37ca8
Mateusz Wasilewski Ubuntu <mateuszwasilewski12@gmail.com>
2022-11-30 08:59:57 +0100

    Converted Animal list to use TableWithTags

--- frontend/src/page/content/animal_list.rs
+++ frontend/src/page/content/animal_list.rs
@@ -3,6 +3,7 @@ use yew::{html, Html, function_component};
 use common::Animal;
 use common::animal::AnimalStatus;
 use crate::page::routes::AnimalLink;
+use crate::common::table::{RowProps, TableWithTags};
 
 fn get_animal_link(id: &str) -> Html {
     html! {
@@ -10,84 +11,58 @@ fn get_animal_link(id: &str) -> Html {
     }
 }
 
-fn animal_tags() -> Html {
-    html! {
-        <div class="row fst-italic">
-            <div class="col">
-                { "id osobnika" }
-            </div>
-            <div class="col">
-                { "nr miotu" }
-            </div>
-            <div class="col-3">
-                { "fenotyp" }
-            </div>
-            <div class="col">
-                { "status" }
-            </div>
-            <div class="col">
-                { "ojciec" }
-            </div>
-            <div class="col">
-                { "matka" }
-            </div>
-        </div>
-    }
+fn animal_tags() -> RowProps {
+    vec! [
+        "id osobnika".into(),
+        "nr miotu".into(),
+        "fenotyp".into(),
+        "status".into(),
+        "ojciec".into(),
+        "matka".into(),
+    ]
 }
 
-fn animal_to_html(animal: &Animal) -> Html {
+fn animal_to_row(animal: &Animal) -> RowProps {
     let litter = &animal.litter;
-
-    html! {
-        <div class="row border-top">
-            <div class="col">
-                {get_animal_link(&animal.id)}
-            </div>
-            <div class="col">
-                { format!("{}", match litter {
-                    Some(litter) => litter.id.to_string(),
-                    None => "--".to_owned()
-                }) }
-            </div>
-            <div class="col-3">
-                { format!("{}", animal.fenotyp) }
-            </div>
-            <div class="col">
-                { match &animal.status {
-                    AnimalStatus::Alive => "zwierzętarnia",
-                    AnimalStatus::Dead => "martwy",
-                    AnimalStatus::Unknown => "nieznany",
-                    AnimalStatus::Adopted => "adopcja"
-                } }
-            </div>
-            <div class="col">
-                { match litter {
-                    Some(litter) => get_animal_link(&litter.father),
-                    None => html! { {{"--".to_owned()}} }
-                } }
-            </div>
-            <div class="col">
-                { match litter {
-                    Some(litter) => get_animal_link(&litter.mother),
-                    None => html! { {{"--".to_owned()}} }
-                } }
-            </div>
-        </div>
-    }
+    vec![
+        get_animal_link(&animal.id),
+        match litter {
+            Some(litter) => litter.id.into(),
+            None => "--".into()
+        },
+        animal.fenotyp.clone().into(),
+        match &animal.status {
+            AnimalStatus::Alive => "zwierzętarnia".into(),
+            AnimalStatus::Dead => "martwy".into(),
+            AnimalStatus::Unknown => "nieznany".into(),
+            AnimalStatus::Adopted => "adopcja".into()
+        },
+        match litter {
+            Some(litter) => get_animal_link(&litter.father),
+            None => "--".into()
+        },
+        match litter {
+            Some(litter) => get_animal_link(&litter.mother),
+            None => "--".into()
+        }
+    ]
 }
 
 #[function_component(AnimalList)]
 pub fn get_animal_list() -> Html {
-    let animal_list: Html = backend_api::get_all_animal().iter()
-            .map(animal_to_html).collect();
+    //let animal_list: Html = backend_api::get_all_animal().iter()
+    //        .map(animal_to_html).collect();
+
+    let animal_list: Vec<RowProps> = backend_api::get_all_animal().iter()
+        .map(animal_to_row).collect();
+    let tags = animal_tags();
 
     html! {
         <>
             <h2>{ "Lista Myszy" }</h2>
 
             <div id="animal_list">
-                {animal_tags()}
-                {animal_list}
+                <TableWithTags tags={tags} data={animal_list} />
             </div>
         </>
     }
